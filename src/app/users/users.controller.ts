1d70ff2e490d098b1d0540394c546372bc1e9642
Louis Rey <rlhardrock@gmail.com>
2025-03-27 16:55:20 -0500

    User Resource C+M+S

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  /* @UseGuards(AuthGuard('jwt')) */
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  /* @UseGuards(AuthGuard('jwt')) */
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Get(':taxpayerId')
+  /* @UseGuards(AuthGuard('jwt')) */
+  findOne(@Param('taxpayerId') taxpayerId: string) {
+    return this.usersService.findOne(taxpayerId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Patch(':taxpayerId')
+  /* @UseGuards(AuthGuard('jwt')) */
+  update(
+    @Param('taxpayerId') taxpayerId: string,
+    @Body() updateUserDto: UpdateUserDto
+  ) {
+    return this.usersService.update(Number(taxpayerId), updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Delete(':taxpayerId')
+  /* @UseGuards(AuthGuard('jwt')) */
+  remove(@Param('taxpayerId') taxpayerId: string) {
+    return this.usersService.remove(Number(taxpayerId));
   }
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { async } from 'rxjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  // Crear un nuevo usuario
+  async create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: {
+        sex: createUserDto.sex,
+        name: createUserDto.name,
+        lastName: createUserDto.lastName,
+        phone: createUserDto.phone,
+        taxpayerId: createUserDto.taxpayerId,
+        email: createUserDto.email,
+        password: createUserDto.password,
+        role: createUserDto.role,
+        status: createUserDto.status,
+        dateBirth: createUserDto.dateBirth,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  // Encuentra todos los usuarios
+  async findAll() {
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  // Encuentra un usuario especifico por Identificación Real ID
+  async findOne(taxpayerId: string) {
+    return this.prisma.user.findUnique({
+      where: { taxpayerId },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  // Actualizar un usuario existente
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        sex: updateUserDto.sex,
+        name: updateUserDto.name,
+        lastName: updateUserDto.lastName,
+        phone: updateUserDto.phone,
+        taxpayerId: updateUserDto.taxpayerId,
+        email: updateUserDto.email,
+        password: updateUserDto.password,
+        role: updateUserDto.role,
+        status: updateUserDto.status,
+        dateBirth: updateUserDto.dateBirth,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // Eliminar un usuario existente
+  async remove(id: number) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
